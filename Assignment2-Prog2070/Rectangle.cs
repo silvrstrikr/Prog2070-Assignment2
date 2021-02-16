047c7c5f2744ea32dc478d21edd46532bdff1983
silvrstrikr <bealsman@gmail.com>
2021-02-15 22:58:05 -0500

    Added Rectangle Class

---
+++ Assignment2-Prog2070/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Prog2070
+{
+    class Rectangle
+    {
+        private int width;
+
+        private int length;
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int length)
+        {
+            return this.length = length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            return this.width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return length * 2 + width * 2;
+        }
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
