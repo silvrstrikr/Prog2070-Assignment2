62e9f3e142046cd57e9b605b4c8a9abfe8dd7c28
silvrstrikr <bealsman@gmail.com>
2021-02-15 23:15:31 -0500

    Get Dimension Completed

--- Assignment2-Prog2070/Program.cs
+++ Assignment2-Prog2070/Program.cs
@@ -10,7 +10,35 @@ namespace Assignment2_Prog2070
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetDimension("Length"));
+        }
 
+        static int GetDimension(string dimensionName)
+        {
+            bool invalid = true;
+            string input;
+            int dimension;
+            do
+            {
+                Console.WriteLine("Please enter Rectangle " + dimensionName + " : Must be > 0");
+                input = Console.ReadLine();
+                if (!int.TryParse(input, out dimension))
+                {
+                    Console.WriteLine("You Must enter a Numeric value");
+                }
+                else if (dimension < 1)
+                {
+                    Console.WriteLine("Your number must be greater than 0");
+                }
+                else
+                {
+                    invalid = false;
+                }
+
+            } while (invalid);
+
+            
+            return dimension;
         }
     }
 }
